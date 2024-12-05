c5f2f5f718b736ab65a8df0d1423fbb2f851feaa
duonghiepit <duonghiep59.it@gmail.com>
2024-12-05 17:47:47 +0700

    configure

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+# Set upload folder and allowed extensions
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+OUTPUT_FOLDER = os.path.join(os.getcwd(), 'outputs')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
